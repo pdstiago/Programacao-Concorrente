a90abf24af7e96618691ed42850e3bdb9d48209f
pdstiago <tiago.pds.silva@gmail.com>
2024-10-07 18:02:00 -0300

    submissão do laboratório 6

---
+++ Laboratório 6/README.md
@@ -0,0 +1,129 @@
+### Como compilar:
+
+gcc -o lab6 lab6.c list_int.c -Wall
+
+### Como executar:
+
+./lab6 numDeThreads
+
+### Demonstrações da prioridade de escrita:
+
+#### Parte do primeiro log com 5 threads:
+Thread 1 está escrevendo
+Fila de leitura: list = 
+Fila de escrita: list = 
+
+Thread 4 está esperando para escrever
+Fila de leitura: list = 
+Fila de escrita: list = 4 
+
+Thread 0 está esperando para escrever
+Fila de leitura: list = 
+Fila de escrita: list = 0 4 
+
+Thread 3 está esperando para ler
+Fila de leitura: list = 3 
+Fila de escrita: list = 0 4 
+
+Thread 1 parou de escrever
+
+Thread 1 está esperando para ler
+Fila de leitura: list = 1 3 
+Fila de escrita: list = 0 4 
+
+Thread 0 está escrevendo
+Fila de leitura: list = 1 3 
+Fila de escrita: list = 4 
+
+Thread 0 parou de escrever
+
+Thread 0 está esperando para ler
+Fila de leitura: list = 0 1 3 
+Fila de escrita: list = 4 
+
+Thread 2 está esperando para ler
+Fila de leitura: list = 0 1 2 3 
+Fila de escrita: list = 4 
+
+Thread 4 está escrevendo
+Fila de leitura: list = 0 1 2 3 
+Fila de escrita: list = 
+
+Thread 4 parou de escrever
+
+Thread 4 está lendo
+Fila de leitura: list = 0 1 2 3 
+Fila de escrita: list = 
+
+Thread 4 parou de ler
+
+#### Parte do segundo log com 7 threads:
+Thread 5 está escrevendo
+Fila de leitura: list = 
+Fila de escrita: list = 
+
+Thread 0 está esperando para escrever
+Fila de leitura: list = 
+Fila de escrita: list = 0 
+
+Thread 4 está esperando para escrever
+Fila de leitura: list = 
+Fila de escrita: list = 0 4 
+
+Thread 1 está esperando para ler
+Fila de leitura: list = 1 
+Fila de escrita: list = 0 4 
+
+Thread 3 está esperando para ler
+Fila de leitura: list = 1 3 
+Fila de escrita: list = 0 4 
+
+Thread 6 está esperando para ler
+Fila de leitura: list = 1 3 6 
+Fila de escrita: list = 0 4 
+
+Thread 2 está esperando para ler
+Fila de leitura: list = 1 2 3 6 
+Fila de escrita: list = 0 4 
+
+Thread 5 parou de escrever
+
+Thread 5 está esperando para ler
+Fila de leitura: list = 1 2 3 5 6 
+Fila de escrita: list = 0 4 
+
+Thread 0 está escrevendo
+Fila de leitura: list = 1 2 3 5 6 
+Fila de escrita: list = 4 
+
+Thread 0 parou de escrever
+
+Thread 0 está esperando para ler
+Fila de leitura: list = 0 1 2 3 5 6 
+Fila de escrita: list = 4 
+
+Thread 4 está escrevendo
+Fila de leitura: list = 0 1 2 3 5 6 
+Fila de escrita: list = 
+
+Thread 4 parou de escrever
+
+Thread 4 está escrevendo
+Fila de leitura: list = 0 1 2 3 5 6 
+Fila de escrita: list = 
+
+Thread 4 parou de escrever
+
+Thread 4 está lendo
+Fila de leitura: list = 0 1 2 3 5 6 
+Fila de escrita: list = 
+
+Thread 4 parou de ler
+
+Thread 4: in=1 out=1 read=1
+
+Thread 3 está lendo
+Fila de leitura: list = 0 1 2 5 6 
+Fila de escrita: list = 
+
+Thread 3 parou de ler
---
+++ Laboratório 6/lab6.c
@@ -0,0 +1,188 @@
+//Programa concorrente que cria e faz operacoes sobre uma lista de inteiros
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_int.h"
+#include <pthread.h>
+
+#define QTDE_OPS 50 //quantidade de operacoes sobre a lista (insercao, remocao, consulta)
+#define QTDE_INI 100 //quantidade de insercoes iniciais na lista
+#define MAX_VALUE 100 //valor maximo a ser inserido
+
+//lista compartilhada iniciada 
+struct list_node_s *head_p = NULL, *head_lei = NULL, *head_esq = NULL;
+//qtde de threads no programa
+int nthreads;
+
+//lock de exclusao mutua
+pthread_mutex_t mutex;
+
+pthread_cond_t cond;
+
+int lei, esq, esqEsperando;
+
+void printarFilas(){
+    printf("Fila de leitura: ");
+    Print(head_lei);
+
+    printf("Fila de escrita: ");
+    Print(head_esq);
+
+    printf("\n");
+}
+
+void entraLeitura(int id){
+    pthread_mutex_lock(&mutex);
+
+    int entrou = 0;
+    while(esq || esqEsperando){
+        if(!entrou){
+            printf("Thread %d está esperando para ler\n", id);
+            Insert(id, &head_lei);
+            printarFilas();
+            entrou = 1;
+        }
+        pthread_cond_wait(&cond, &mutex);
+    }
+
+    if(entrou) Delete(id, &head_lei);
+
+    printf("Thread %d está lendo\n", id);
+    printarFilas();
+
+    lei++;
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void saiLeitura(int id){
+    pthread_mutex_lock(&mutex);
+    printf("Thread %d parou de ler\n\n", id);
+    lei--;
+    pthread_mutex_unlock(&mutex);
+}
+
+void entraEscrita(int id){
+    pthread_mutex_lock(&mutex);
+
+    int entrou = 0;
+
+    while(esq){
+        if(!entrou){
+            esqEsperando++;
+            entrou = 1;
+            Insert(id, &head_esq);
+            printf("Thread %d está esperando para escrever\n", id);
+            printarFilas();
+        }
+        pthread_cond_wait(&cond, &mutex);
+    }
+
+    if(entrou){
+        Delete(id, &head_esq);
+        esqEsperando--;
+    }
+
+    printf("Thread %d está escrevendo\n", id);
+    printarFilas();
+
+    esq++;
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void saiEscrita(int id){
+    pthread_mutex_lock(&mutex);
+
+    printf("Thread %d parou de escrever\n\n", id);
+
+    esq--;
+
+    pthread_cond_broadcast(&cond);
+
+    pthread_mutex_unlock(&mutex);
+}
+
+//tarefa das threads
+void* tarefa(void* arg) {
+    long int id = (long int) arg;
+    int op;
+    int in, out, read; 
+    in=out=read = 0; 
+
+    //realiza operacoes de consulta (80%), insercao (10%) e remocao (10%)
+    for(long int i=id; i<QTDE_OPS; i+=nthreads) {
+        op = rand() % 100;
+        if(op<80){
+            entraLeitura(id); /* lock de LEITURA */    
+            Member(i%MAX_VALUE, head_p);   /* Ignore return value */
+            saiLeitura(id);
+            read++;
+        } else if(80<=op && op<90) {
+            entraEscrita(id); /* lock de ESCRITA */    
+            Insert(i%MAX_VALUE, &head_p);  /* Ignore return value */
+            saiEscrita(id);
+            in++;
+        } else if(op>=90) {
+            entraEscrita(id); /* lock de ESCRITA */     
+            Delete(i%MAX_VALUE, &head_p);  /* Ignore return value */
+            saiEscrita(id);
+            out++;
+        }
+    }
+    //registra a qtde de operacoes realizadas por tipo
+    printf("Thread %ld: in=%d out=%d read=%d\n\n", id, in, out, read);
+    pthread_exit(NULL);
+}
+
+/*-----------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+    pthread_t *tid;
+    
+    //verifica se o numero de threads foi passado na linha de comando
+    if(argc<2) {
+        printf("Digite: %s <numero de threads>\n", argv[0]); return 1;
+    }
+    nthreads = atoi(argv[1]);
+
+    //insere os primeiros elementos na lista
+    for(int i=0; i<QTDE_INI; i++)
+        Insert(i%MAX_VALUE, &head_p);  /* Ignore return value */
+    
+
+    //aloca espaco de memoria para o vetor de identificadores de threads no sistema
+    tid = malloc(sizeof(pthread_t)*nthreads);
+    if(tid==NULL) {  
+        printf("--ERRO: malloc()\n"); return 2;
+    }
+
+    //inicializa a variavel mutex
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+    
+    //cria as threads
+    for(long int i=0; i<nthreads; i++) {
+        if(pthread_create(tid+i, NULL, tarefa, (void*) i)) {
+            printf("--ERRO: pthread_create()\n"); return 3;
+        }
+    }
+    
+    //aguarda as threads terminarem
+    for(int i=0; i<nthreads; i++) {
+        if(pthread_join(*(tid+i), NULL)) {
+            printf("--ERRO: pthread_join()\n"); return 4;
+        }
+    }
+
+    //libera o mutex
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond);
+    //libera o espaco de memoria do vetor de threads
+    free(tid);
+    //libera o espaco de memoria da lista
+    Free_list(&head_p);
+    Free_list(&head_lei);
+    Free_list(&head_esq);
+
+    return 0;
+}  /* main */
