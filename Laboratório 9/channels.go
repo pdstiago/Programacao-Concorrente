83a734fe1c61b499e82321640fc70420b6e0f5cf
pdstiago <tiago.pds.silva@gmail.com>
2024-10-28 20:41:14 -0300

    submissão Laboratório 9

---
+++ Laboratório 9/channels.go
@@ -0,0 +1,44 @@
+//introduz canais na aplicação Go
+package main
+
+import (
+	"fmt"
+)
+
+func tarefa(str chan string) {
+	msg := <-str
+	fmt.Println(msg, "(impresso pela Goroutine)")
+	str <- "Oi Main, bom dia, tudo bem?"
+
+	msg = <-str
+	fmt.Println(msg, "(impresso pela Goroutine)")
+	str <- "Certo, entendido."
+
+	msg = <-str
+	fmt.Println(msg, "(impresso pela Goroutine)")
+	str <- "finalizando"
+}
+
+func main() {
+	var msg = "" //string vazia
+	
+	//cria um canal de comunicacao nao-bufferizado
+	str := make(chan string)
+	
+	//cria um goroutine que executará a função 'tarefa'
+	go tarefa(str)
+	
+	str <- "Olá, Goroutine, bom dia!"
+	msg = <-str
+	fmt.Println(msg, "(impresso pela Main)")
+	
+
+	str <- "Tudo bem! Vou terminar tá?"
+	msg = <-str
+	fmt.Println(msg, "(impresso pela Main)")
+
+
+	str <- "finalizando"
+	msg = <-str
+	fmt.Println(msg, "(impresso pela Main)")
+}
---
+++ Laboratório 9/primos.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func ehPrimo(n int) int {
+	if n<=1 {
+		return 0
+	}
+	if n == 2 {
+		return 1
+	}
+	if n%2 == 0 {
+		return 0
+	}
+	for i:=3; i<int(math.Sqrt(float64(n)))+1; i+=2 {
+		if n%i == 0 {
+			return 0
+		}
+	}
+	return 1
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Digite: %s <N> <M>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	N, errN := strconv.Atoi(os.Args[1])
+	M, errM := strconv.Atoi(os.Args[2])
+
+	if errN != nil || errM != nil {
+		fmt.Println("--ERRO: <N> e <M> devem ser inteiros")
+		os.Exit(1)
+	}
+
+    numeros := make(chan int, N)
+	resultadosEhPrimo := make(chan int, N)
+
+	for i:=0; i<M; i++ {
+		go func() {
+			for {
+				j, more := <-numeros
+				if !more {
+					return
+				}
+				resultadosEhPrimo <- ehPrimo(j)
+			}
+		}()
+	}
+
+	for i:=1; i<=N; i++ {
+		numeros<-i
+	}
+	close(numeros)
+
+    primos:=0
+
+	for i:=1; i<=N; i++ {
+		primos += <-resultadosEhPrimo
+	}
+
+	fmt.Println("Quantidade total de números primos encontrados:", primos)
+}
